ff7d1ebbacce539d1d89d2c4a6b1a2830532b8e8
Robert Cloa <rumlowb@gmail.com>
2024-01-19 17:32:52 -0800

    commit: revamping UI of roomsModal

--- components/body/rooms/rooms-display/RoomsDisplay.js
+++ components/body/rooms/rooms-display/RoomsDisplay.js
@@ -93,7 +93,7 @@ function RoomsDisplay({roomId, doc}) {
     </div>
     {roomModal && (
       <div className="
-      h-[100vh]
+      h-screen
       w-screen
       bg-slate-800
       bg-opacity-80
@@ -104,318 +104,64 @@ function RoomsDisplay({roomId, doc}) {
       fixed
       ">
         <div className="
-       h-[85vh] 
-       lg:w-[60vw]
-       w-[95vw]
-       flex
-       flex-col
-       rounded
-       bg-slate-700
-       space-y-3
+        roomsDisplayModal
         ">
           <header className="
-          h-[60px]
+          h-[50px]
           top-0
-          sticky
           z-50
+          sticky
           flex
           items-center
           justify-between
-          px-4
-          py-3
           border-b
           border-amber-600
-          ">
-            <h1 className="
-            font-path-ex
-            font-bold
-            text-lg
-            text-amber-500
-            ">
-              Info about {doc.roomName}
-            </h1>
-            <button 
-            onClick={() => setRoomModal(false)}
-            className="
-            text-xl
-            font-fira-sans
-            font-black
-            text-amber-600
-            border
-            rounded-full
-            px-3
-            py-1
-            border-amber-600
-            hover:text-amber-700
-            hover:border-2
-            hover:border-amber-700
-            transform
-            transition
-            duration-300
-            ease-in-out
-            ">
-              X
-            </button>
-          </header>
-          <div className="
-          h-[85%]
+          px-3
+          py-2
+          "></header>
+          <main className="
+          h-[90%]
           w-full
-          bg-slate-700
-          bg-opacity-80
           flex
           flex-col
-          items-start
-          space-y-4
+          space-y-3
+          px-3
+          py-2
           ">
-                
-              {/*room image here */}
-            <h1 className="
-            text-3xl
-            mx-auto
-            text-amber-700
-            font-montserr
-            font-bold
-            ">
-              {doc.roomName}
-            </h1>
-        
-            <div className="
-            roomsDisplayModalTop
-            ">
-            <span className="
-            roomsDisplaySpan
-            ">
-              <h1 className="
-            text-lg
-            text-amber-500
-            font-fira-sans
-            font-bold
-            ">
-              Room creator:
-            </h1>
-            <h1 className="
-            text-xl
-            text-amber-700
-            font-montserr
-            font-bold
-            ">
-              {doc.creator}
-            </h1>
-            </span>
-              <span className="
-              roomsDisplaySpan
-              ">
-                <h1 className="
-                text-lg
-                text-amber-500
-                font-fira-sans
-                font-bold
-                ">
-                  Created on:
-                </h1>
-                <h1 className="
-                text-xl
-                text-amber-700
-                font-montserr
-                font-bold
-                ">
-                  {new Date((doc.createdOn)?.toDate()).toLocaleString()}
-                </h1>
-              </span>
-            </div>
-            {/**end of roomsDisplayTop */}
-            {/**roomsDisplay description */}
-            <span className="roomsDisplayDescSpan
-            ">
-              <h2 className="
-              font-montserr
-              font-normal
-              text-xl
-              text-amber-400
-              underline
-              ">
-                Room description
-              </h2>
-              <h4 className="
-              font-montserr
-              font-bold
-              text-base
-              text-amber-600
-              ">
-                {doc?.roomDesc}
-              </h4>
-            </span>
-            {/**roomsDisplay desc end */}
-            {/**members add form */}
-            {(user?.displayName == doc?.creator || user?.email == 'rumlowb@gmail.com') && (
-              <>
-              <div className="
-              w-full
-              rounded-md
-              border
-              border-amber-500
-              bg-slate-800
-              space-y-2
-              ">
-                <input
-                value={memberEmail}
-                onChange={e => setMemberEmail(e.target.value)} 
-                placeholder={`Add a member here, ${user?.displayName}`}
-                className='
-                px-3
-                py-4
-                my-2
-                w-full
-                bg-inherit
-                text-amber-600
-                font-path-ex
-                font-semibold
-                placeholder-amber-500
-                placeholder-opacity-60
-                focus:outline-none
-                '></input>
-                <span className="
-                h-[35%]
-                flex
-                items-center
-                justify-between
-                px-3
-                py-3
-                my-2
-                w-full
-                border-t-2
-                border-amber-500
-                ">
-                  <h1></h1>
-                  <button 
-                  onClick={addMember}
-                  className="
-                  bg-slate-800
-                  border
-                  border-amber-500
-                  text-amber-500
-                  font-path-ex
-                  font-semibold
-                  text-lg
-                  px-4
-                  py-2
-                  my-2
-                  hover:border-amber-700
-                  hover:text-amber-700
-                  hover:font-bold
-                  transform
-                  transition
-                  ease-in-out
-                  delay-100
-                  hover:-skew-x-6
-                  ">
-                    Add member
-                  </button>
-                </span>
-              </div>
-              {/**membersList */}
-              <div className="
-              h-full
-              w-full
-              flex
-              flex-col
-              items-start
-              bg-slate-900
-              rounded
-              ">
-                <header className="
-                top-0
-                z-50
-                sticky
-                px-3
-                py-2
-                mb-3
-                bg-slate-700
-                border-b-2
-                border-amber-500
-                h-[10%]
-                w-full
-                ">
-                  <h3 className="
-                  font-path-ex
-                  font-semibold
-                  text-amber-500
-                  text-lg
-                  ">
-                    Members of {doc?.roomName}
-                  </h3>
-                </header>
-                <div className="
-                h-full
-                w-full
-                overflow-y-scroll
-                scrollbar-thin
-                scrollbar-track-slate-700
-                scrollbar-thumb-amber-500
-                ">
-                  {membersList && membersList?.docs.map(doc => (
-                    <RoomMember 
-                    userEmail={doc?.data()?.memberEmail}
-                    doc={doc?.data()}
-                    />
-                  ))}
-                </div>
-              </div>
-              {/**end of membersList */}
-              </>
-            )}
-            {/**members add form end */}
-          </div>
-          {/**footer */}
+            {/**
+             * 
+             * roomsDisplay modal top with forms and details
+             * 
+             *  */}
+            {/**
+             * 
+             * end of roomsDisplay modal
+             * 
+             *  */}
+                         {/**
+             * 
+             * roomsDisplay modal bottom with members
+             * 
+             *  */}
+            {/**
+             * 
+             * end of roomsDisplay modal with members
+             * 
+             *  */}
+          </main>
           <footer className="
-          h-[60px]
-          w-full
+          h-[50px]
           bottom-0
-          sticky
           z-50
-          border-t-2
-          border-amber-600
+          sticky
           flex
           items-center
           justify-between
-          ">
-            <span className='
-            w-[50%]
-            '></span>
-            <span className="
-            px-4
-            py-3
-            w-[50%]
-            ">
-              <button 
-              onClick={() => router.push(`/rooms/${roomId}`)}
-              className="
-              bg-slate-900
-              font-montserr
-              font-semibold
-              text-lg
-              h-[90%]
-              px-3
-              py-2
-              rounded
-              w-[70%]
-              text-amber-500
-              hover:bg-opacity-90
-              hover:text-amber-600
-              hover:border-x-4
-              hover:border-y
-              hover:border-amber-600
-              -inset-full
-              transform
-              transition
-              delay-100
-              ease-in-out
-              ">
-                To room
-              </button>
-            </span>
-          </footer>
-          {/**end of footer */}
+          px-3
+          py-2
+          border-t
+          border-amber-600
+          " ></footer>
         </div>
       </div>
     )}
